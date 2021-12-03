27bdddc1a88e165b7cecca56e821e31cb8aa29a5
bitbucket-pipelines <commits-noreply@bitbucket.org>
2021-12-03 05:05:00 +0000

    Release v0.0.1

--- .devcontainer/startContainer.sh
+++ .devcontainer/startContainer.sh
@@ -12,6 +12,10 @@ conda activate anatools && \
 pip install anatools >> ./.devcontainer/startContainer.log
 echo "done."
 
+# set environment variables
+echo "ANA_ROOT='/workspaces/ana'" >> /home/$USERNAME/.bashrc
+echo "DATA_ROOT='/workspaces/ana/ana/data'" >> /home/$USERNAME/.bashrc
+
 # expose the docker socket
 sudo chmod 777 /var/run/docker.sock 
 docker logout public.ecr.aws
